729e5bc42405d806863e8f93b3ef10de050e5433
Eldjons <127309499+Eldjons@users.noreply.github.com>
2023-08-25 15:46:15 +0700

    feat: create payment controller feat: add SequelizeUniqueConstraintError, SequelizeDatabaseError in error Handler feat: add schedules table and remove it from schedules column from Package table feat: Seed schedules data

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 const errorHandler = async (err, request, response, next) => {
+  console.log(err, "err handler 888888");
   if (err.name === "Unauthenticated") {
     response.status(401).json({
       message: "Error Authentication",
@@ -12,12 +13,25 @@ const errorHandler = async (err, request, response, next) => {
     response.status(404).json({
       message: "Data not found",
     });
+  } else if (err.name === "TxNotFound") {
+    response.status(404).json({
+      message: "Transaction not found",
+    });
+  } else if (err.name === "PxNotFound") {
+    response.status(404).json({
+      message: "Package not found",
+    });
   } else if (err.name === "Unauthorized") {
     response.status(403).json({
       message: "Forbidden Error Authorization",
     });
-  } else if (err.name === "SequelizeValidationError") {
-    response.status(400).json(err.errors[0].message);
+  } if (err.name === "SequelizeValidationError" || err.name === "SequelizeUniqueConstraintError" || err.name === "SequelizeDatabaseError") {
+    const errors = err.errors || [];
+    let messages = [];
+    for (const errorMessages of errors) {
+      messages.push(errorMessages.message);
+    }
+    response.status(400).json({ message: messages });
   } else if (err.name === "ErrorData") {
     response.status(404).json({
       message: "Error not found",
@@ -33,6 +47,11 @@ const errorHandler = async (err, request, response, next) => {
   } else if (err.name == "Invalid") {
     response.status(401).json({
       message: "error invalid username or email or password",
+    });
+  } else if (err.name === "NodemailerFail") {
+    response.status(422).json({
+      message: "Nodemailer failed to send the email",
+      error: err.message, // Include the actual error message from Nodemailer
     });
   } else {
     response.status(500).json({
